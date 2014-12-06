1fc2d39870ff022dafe44f92cb885114f6f5cf34
Dzmitry Ananchuk <psyhogenesis@gmail.com>
2014-12-06 17:10:09 +0100

    Dodano Hex.java, UML poprawiony.

--- src/Elements/Hex.java
+++ src/Elements/Hex.java
@@ -2,5 +2,30 @@ package Elements;
 
 public class Hex 
 {
+	private Creature animal = null;
+	
+	
+	public Creature getCreature(){
+		return animal;
+	}
+	
+	
+	public void setCreature(Creature a_animal){
+		this.animal = a_animal;
+	}
+	
+	
+	public boolean isOccupied(){
+		if (animal != null) 
+        {
+            return true;
+        }
+        return false;
+	}
+	
+	
+	public void clear(){
+		animal = null;
+	}
 
 }
